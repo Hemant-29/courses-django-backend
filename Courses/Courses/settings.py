bbd120180681ae23fdce1d4a5db6b44b4bf360e9
Hemant <68590578+Hemant-29@users.noreply.github.com>
2024-08-17 02:18:40 +0530

    name changes

--- Courses/Courses/settings.py
+++ Courses/Courses/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-nt-3is2ek&oijg-fq8sntw%qisr@5x6c(qnzbpp5r)x-*m-yh$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "courseApp",
+    "course_api",
 ]
 
 MIDDLEWARE = [
