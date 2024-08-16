0c26ab817047ee586d78bdda0d5f87363d62a3b1
Hemant <68590578+Hemant-29@users.noreply.github.com>
2024-08-16 06:21:18 +0530

    first commit

---
+++ Courses/courseApp/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Course, CourseInstance
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseInstanceSerializer(serializers.ModelSerializer):
+    course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all())
+
+    class Meta:
+        model = CourseInstance
+        fields = '__all__'
